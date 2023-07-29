e7ea526966676543429d1653090be687b9b90cc7
ElenaBarker <113217019+ElenaBarker@users.noreply.github.com>
2023-07-29 04:54:05 +0100

    get all albums and album by ID

--- server.js
+++ server.js
@@ -1,24 +1,41 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function (req, res) {
-  let value1 = req.query.value1;
-  let value2 = req.query.value2;
-  let mult = value1 * value2;
-  res.send(`The multiply of ${value1} and ${value2} equals ${mult}`);
+app.use(express.json());
+
+const albumsData = [
+  {
+    albumId: "10",
+    artistName: "Beyoncé",
+    collectionName: "Lemonade",
+    artworkUrl100:
+      "http://is1.mzstatic.com/image/thumb/Music20/v4/23/c1/9e/23c19e53-783f-ae47-7212-03cc9998bd84/source/100x100bb.jpg",
+    releaseDate: "2016-04-25T07:00:00Z",
+    primaryGenreName: "Pop",
+    url: "https://www.youtube.com/embed/PeonBmeFR8o?rel=0&amp;controls=0&amp;showinfo=0",
+  },
+  {
+    albumId: "11",
+    artistName: "Beyoncé",
+    collectionName: "Dangerously In Love",
+    artworkUrl100:
+      "http://is1.mzstatic.com/image/thumb/Music/v4/18/93/6d/18936d85-8f6b-7597-87ef-62c4c5211298/source/100x100bb.jpg",
+    releaseDate: "2003-06-24T07:00:00Z",
+    primaryGenreName: "Pop",
+    url: "https://www.youtube.com/embed/ViwtNLUqkMY?rel=0&amp;controls=0&amp;showinfo=0",
+  },
+];
+
+app.get("/albums", function (req, res) {
+  res.send({ albumsData });
 });
 
-app.get("/chocolate", function (req, res) {
-  let amount = req.query.amount;
-  res.send(`Here is your yummy ${amount} chocolate(s) :) `);
-});
-
-app.get("/node", function (req, res) {
-  res.send("Yay Node ;)");
-});
-
-app.get("/codeyourfuture", function (req, res) {
-  res.send("Welcome to the best bootcamp in the UK :)");
+app.get("/albums/:albumId", function (req, res) {
+  const albumToFind = req.params.albumId;
+  console.log(req.params.albumId);
+  const album = albumsData.find((album) => album.albumId === albumToFind);
+  //console.log(album);
+  res.status(200).send({ album });
 });
 
 app.listen(3000, function () {
